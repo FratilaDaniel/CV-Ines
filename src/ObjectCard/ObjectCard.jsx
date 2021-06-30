d8d971c61e268a7faf32d6bb6e7258090e639799
FratilaDaniel <danfratila1997@gmail.com>
2021-06-30 20:25:27 +0300

    add custom zoom on component

--- src/ObjectCard/ObjectCard.jsx
+++ src/ObjectCard/ObjectCard.jsx
@@ -1,14 +1,21 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Color, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import React from "react";
 import lionPath from "../resources3D/lion_wL_3.glb";
+
+const MIN_ZOOM_VALUE = 3;
+const INITIAL_ZOOM_VALUE = 7;
+const MAX_ZOOM_VALUE = 10;
+const NORMALIZATION_FACTOR = 10;
+
 
 class ObjectCard extends React.Component{
     constructor(){
         super();
         this.state = {
             isMouseOver: false,
-            animationStarted: true
+            animationStarted: true,
+            zoomLevel: INITIAL_ZOOM_VALUE
         };
     }
 
@@ -35,15 +42,15 @@ class ObjectCard extends React.Component{
         }, 
         undefined, // loading animation/text function 
         function(e){
-            console.log("AAAAAAAAAAAAAAAAAA");
-            console.log(e);
+            console.log("AAAAAAAAAAAAAAAAAA", e);
         });
 
         camera.rotation.x = -0.5;
         camera.position.y = 5;
-        camera.position.z = 7;
 
         this.startAnimation = () => {
+            camera.position.z = this.state.zoomLevel;
+
             if(model && !this.state.isMouseOver){
                 model.rotation.y += 0.01;
             }
@@ -67,12 +74,32 @@ class ObjectCard extends React.Component{
         });
     }
 
+    handleWheel(e){
+        // should prevent default (page scroll)
+        const changeValue = e.deltaY;
+        this.setState((oldState) => {
+            let newZoomValue = oldState.zoomLevel + Math.sign(changeValue) / NORMALIZATION_FACTOR;
+            if(newZoomValue < MIN_ZOOM_VALUE){
+                newZoomValue = MIN_ZOOM_VALUE;
+            }
+            else if(newZoomValue > MAX_ZOOM_VALUE){
+                newZoomValue = MAX_ZOOM_VALUE
+            }
+            return {
+                zoomLevel: newZoomValue
+            }
+        });
+    }
+
 
     render(){
         return (
-            <div ref={ref => (this.mount = ref)}
+            <div
+                className="card-div" 
+                ref={ref => (this.mount = ref)}
                 onMouseOver={() => this.handleMouseOver()}
                 onMouseOut={() => this.handleMouseOut()}
+                onWheel={(e) => this.handleWheel(e)}
             >
             </div>
         )
