1871a0dfa02b4eaffa3d746ac3a8dcd321ea37b8
FratilaDaniel <danfratila1997@gmail.com>
2021-07-05 15:34:13 +0300

    add art works section, remove some duplicate code

--- src/views/ProjectsArtworks.jsx
+++ src/views/ProjectsArtworks.jsx
@@ -1,10 +1,24 @@
+import React from "react";
+import berdImg1 from "../artworks/berd_img1.jpg";
+import berdImg2 from "../artworks/berd_img2.png";
+import Slideshow from "./Slideshow";
+
+const ART_WORKS_INFO = [{
+    title: "A sexy picture of berd",
+    visual: <img src={berdImg1} alt=""/>
+},{
+    title: "A better view of berd" ,
+    visual: <img src={berdImg2} alt=""/>
+}];
+
 
 function Artworks(){
-
     return (
-        <div className="artworks-container">
-            this is artworks
-        </div>
+        <Slideshow
+            title="ART WORKS"
+            items={ART_WORKS_INFO}
+            descriptionRequired={false}
+        />
     );
 }
 
---
+++ src/views/Slideshow.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+class Slideshow extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sectionTitle: "",
+            activeItemIndex: 0,
+            items: [],
+            descriptionRequired: false
+        };
+    }
+
+    static getDerivedStateFromProps(props, state){
+        return {
+            sectionTitle: props.title,
+            items: props.items,
+            descriptionRequired: props.descriptionRequired
+        };
+    }
+
+    getPreviousIndex(index){
+        if(index > 0){
+            return index - 1;
+        }
+        else{
+            return this.state.items.length - 1;
+        }
+    }
+
+    getNextIndex(index){
+        console.log(index, (index + 1) % this.state.items.length)
+        return (index + 1) % this.state.items.length;
+    }
+
+    onPreviousClick(){
+        this.setState((oldState) => ({
+                activeItemIndex: this.getPreviousIndex(oldState.activeItemIndex)
+        }));
+    }
+    
+    onNextClick(){
+        this.setState(oldState => ({
+            activeItemIndex: this.getNextIndex(oldState.activeItemIndex)
+        }));
+    }
+
+    render(){
+        return (
+            <div className="slideshow-container">
+                <h1>{this.state.sectionTitle}</h1>
+                <div className="slideshow-item-container">
+                    <h2>{this.state.items[this.state.activeItemIndex].title}</h2>
+
+                    <div className="visual-container">
+                        {this.state.items[this.state.activeItemIndex].visual}
+                    </div>
+
+                    <div className="buttons-container">
+                        <button 
+                            className="left-button"
+                            onClick={() => this.onPreviousClick()}></button>
+                        <span>{this.state.activeItemIndex + 1}/{this.state.items.length}</span>
+                        <button 
+                            className="right-button"
+                            onClick={() => this.onNextClick()}></button>
+                    </div>
+
+                    {this.state.descriptionRequired 
+                        ?<div className="description-container">
+                            <p>Description:</p>
+                            <p>{this.state.items[this.state.activeItemIndex].description}</p>
+                        </div> 
+                        : null}
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Slideshow;
